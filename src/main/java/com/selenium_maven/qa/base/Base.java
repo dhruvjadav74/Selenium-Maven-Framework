6454ea6127d61f0576b2fa6507d8c1de20918b5e
dhruvjadav74 <jadav.dhruv.74@gmail.com>
2023-11-01 17:09:48 +0530

    Added framework details

---
+++ src/main/java/com/selenium_maven/qa/base/Base.java
@@ -0,0 +1,52 @@
+package com.selenium_maven.qa.base;
+
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+import java.io.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import com.selenium_maven.qa.utils.Utilities;
+
+public class Base {
+
+	WebDriver driver;
+	public Properties prop;
+
+	public Base() {
+		prop = new Properties();
+		File propFile = new File(
+				System.getProperty("user.dir") + "\\src\\main\\java\\com\\irctc\\qa\\config\\Config.properties");
+		try {
+			FileInputStream fis = new FileInputStream(propFile);
+			prop.load(fis);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	public WebDriver intializeBrowser(String browserName) {
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			driver = new ChromeDriver(options);
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			driver = new FirefoxDriver(options);
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			EdgeOptions options = new EdgeOptions();
+			driver = new EdgeDriver(options);
+		}
+		driver.manage().window().maximize();
+		driver.get(prop.getProperty("url"));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utilities.PAGE_WAIT_TIME));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.IMPLICIT_WAIT_TIME));
+		return driver;
+	}
+}
