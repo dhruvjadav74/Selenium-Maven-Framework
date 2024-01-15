ecff4d9a6fa9cd3cdcd806375c917c2deeb8fe1f
dhruvjadav74 <jadav.dhruv.74@gmail.com>
2024-01-15 14:12:14 +0530

    Removed Utilities SS files Implemented config property to get email & password

--- src/main/java/com/selenium_maven/qa/pages/LoginPage.java
+++ src/main/java/com/selenium_maven/qa/pages/LoginPage.java
@@ -5,13 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
-
-	By username = By.cssSelector("input[placeholder='Email']");
-	By password = By.cssSelector("input[placeholder='Password']");
+	By uname = By.cssSelector("input[placeholder='Email']");
+	By pswd = By.cssSelector("input[placeholder='Password']");
 	By loginbtn = By.xpath("//div[text()='Login']");
 	By contactsbtn = By.xpath("//i[@class='users icon']");
 	By createcontact = By.xpath("//button[text()='Create']");
-
 	By firstname = By.xpath("//input[@name='first_name']");
 	By lastname = By.xpath("//input[@name='last_name']");
 	By middlename = By.xpath("//input[@name='middle_name']");
@@ -21,10 +19,10 @@ public class LoginPage extends BasePage {
 		PageFactory.initElements(driver, this);
 	}
 
-	public void enterUsernameAndPassword() throws InterruptedException {
+	public void enterUsernameAndPassword(String email,String password) throws InterruptedException {
 		Thread.sleep(2000);
-		sendKeys(username, "test0089@mailinator.com");
-		sendKeys(password, "TEst@123456");
+		sendKeys(uname, email);
+		sendKeys(pswd, password);
 	}
 
 	public void clickLogin() {
--- src/test/java/com/selenium_maven/qa/testcases/LoginTest.java
+++ src/test/java/com/selenium_maven/qa/testcases/LoginTest.java
@@ -1,21 +1,17 @@
 package com.selenium_maven.qa.testcases;
-
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.selenium_maven.qa.base.Base;
 import com.selenium_maven.qa.pages.LoginPage;
-
 import io.qameta.allure.Step;
 
 
 public class LoginTest extends Base {
 	
-
 	public LoginTest() {
 		super();
 	}
@@ -27,13 +23,12 @@ public class LoginTest extends Base {
 	public void navigateToBaseUrl() {
 		driver = intializeBrowser(prop.getProperty("browser"));
 		loginPage = new LoginPage(driver);
-		
 	}
 	
 	@Step("Login Test Case")
 	@Test
 	public void Checklogin() throws InterruptedException {
-		loginPage.enterUsernameAndPassword();
+		loginPage.enterUsernameAndPassword(prop.getProperty("email"),prop.getProperty("password"));
 		loginPage.clickLogin();
 		Thread.sleep(2000);
 		loginPage.navigateToContactsPageAndCreate();
@@ -45,6 +40,4 @@ public class LoginTest extends Base {
 		Thread.sleep(1000);
 		driver.close();
 	}
-	
-	
 }	
